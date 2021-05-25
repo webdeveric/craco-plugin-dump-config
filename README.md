d90fa0781fbc7eef6d8ca2c475beca90ca3ff6d1
Eric King <eric@webdeveric.com>
2021-05-25 11:58:27 -0500

    Renamed plugin
    - Updated dependencies
    - Updated eslint config
    - Plugin now uses util.inspect() by default

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# `craco-plugin-dump-config`
+# `craco-plugin-inspect-config`
 
-Dump your Craco, webpack, dev server, and Jest configs to a file so you can inspect them.
+Inspect your Craco, webpack, dev server, and Jest configs then write the output to a text file in the `outputDir`.
 
 ## Install
 
 ```shell
-npm install craco-plugin-dump-config
+npm install craco-plugin-inspect-config -D
 ```
 
 ## Usage
@@ -15,14 +15,14 @@ Add the following to your `craco.config.js` file.
 :warning: The plugin is not enabled by default. You must turn it on when you want to use it.
 
 ```js
-const { dumpConfigPlugin } = require('craco-plugin-dump-config');
+const { inspectConfigPlugin } = require('craco-plugin-inspect-config');
 
 module.exports = {
   // ...
   plugins: [
     // ... put it last
     {
-      plugin: dumpConfigPlugin,
+      plugin: inspectConfigPlugin,
       options: {
         enabled: true,
       },
@@ -34,30 +34,32 @@ module.exports = {
 
 ## Options
 
+This plugin accepts the options from [`util.inspect()`](https://nodejs.org/api/util.html#util_util_inspect_object_options) and the following:
+
 ```
 {
   enabled: boolean;
   outputDir: string | ((pluginOptions) => string);
-  space: number | string;
-  stringify: (config, pluginOptions) => string;
+  stringify: (config, inspectOptions) => string;
   getFilename: (name) => string;
 }
 ```
 
 If you provide your own `stringify`, also provide `getFilename` so you can customize the extension.
 
+Example:
+
 ```js
-const { inspect } = require('util');
-const { dumpConfigPlugin } = require('craco-plugin-dump-config');
+const { inspectConfigPlugin } = require('craco-plugin-inspect-config');
 
 module.exports = {
   plugins: [
     {
-      plugin: dumpConfigPlugin,
+      plugin: inspectConfigPlugin,
       options: {
         enabled: true,
-        stringify: config => inspect(config, { showHidden: false, depth: null, colors: false }),
-        getFilename: name => `${name}.txt`,
+        stringify: config => JSON.stringify(config, null, 2),
+        getFilename: name => `${name}.json`,
       },
     },
   ],
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { inspect, InspectOptions } from 'util';
 import { join, relative } from 'path';
 import { mkdirSync, writeFileSync } from 'fs';
 
@@ -7,34 +8,32 @@ import { log } from '@craco/craco/lib/logger';
 
 import type { AnyRecord, CoreOptions } from '@webdeveric/craco-plugin';
 
-export type PluginOptions = {
+export type CorePluginOptions = {
   outputDir: string | ((pluginOptions: PluginOptions) => string);
-  space: number | string;
-  stringify: <C>(config: C, pluginOptions: Omit<PluginOptions, 'stringify'>) => string;
+  stringify: <C>(config: C, inspectOptions: InspectOptions) => string;
   getFilename: (name: string) => string;
 };
 
-const dumpConfig =
+export type PluginOptions = InspectOptions & CorePluginOptions;
+
+const inspectConfig =
   (name: string) =>
   <C extends AnyRecord, O extends PluginOptions & CoreOptions>(config: C, pluginOptions: O): typeof config => {
     if (pluginOptions.enabled) {
       try {
-        const { stringify, ...otherOptions } = pluginOptions;
+        const { getFilename, outputDir, stringify, ...inspectOptions } = pluginOptions;
 
-        const content = stringify(config, otherOptions);
+        const content = stringify(config, inspectOptions);
 
-        const directory =
-          typeof pluginOptions.outputDir === 'function'
-            ? pluginOptions.outputDir(pluginOptions)
-            : pluginOptions.outputDir;
+        const directory = typeof outputDir === 'function' ? outputDir(pluginOptions) : outputDir;
 
-        const output = join(directory, pluginOptions.getFilename(name));
+        const output = join(directory, getFilename(name));
 
         mkdirSync(directory, { recursive: true });
 
         writeFileSync(output, content);
 
-        log(`dumpConfig(${name}) written to ${relative(process.cwd(), output)}`);
+        log(`inspectConfig(${name}) written to ${relative(process.cwd(), output)}`);
       } catch (error) {
         console.error(error);
       }
@@ -43,20 +42,20 @@ const dumpConfig =
     return config;
   };
 
-export const dumpConfigPlugin = createCracoPlugin<PluginOptions>({
-  name: 'Dump Config',
+export const inspectConfigPlugin = createCracoPlugin<PluginOptions>({
+  name: 'Inspect Config',
   getOptions: options => ({
     enabled: false,
-    space: 2,
-    outputDir: () => findCacheDir({ name: 'craco-plugin-dump-config' }) ?? process.cwd(),
-    stringify: (config, options) => JSON.stringify(config, null, options.space),
-    getFilename: name => `${name}.json`,
+    showHidden: false,
+    depth: null,
+    colors: false,
+    outputDir: () => findCacheDir({ name: 'craco-plugin-inspect-config' }) ?? process.cwd(),
+    stringify: (config, inspectOptions) => inspect(config, inspectOptions),
+    getFilename: name => `${name}.txt`,
     ...options,
   }),
-  craco: [dumpConfig('craco')],
-  webpack: [dumpConfig('webpack')],
-  devServer: [dumpConfig('devServer')],
-  jest: [dumpConfig('jest')],
+  craco: [inspectConfig('craco')],
+  webpack: [inspectConfig('webpack')],
+  devServer: [inspectConfig('devServer')],
+  jest: [inspectConfig('jest')],
 });
-
-export default dumpConfigPlugin;
